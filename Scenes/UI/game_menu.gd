55821957b21faf221d32eb271530534c687a1403
diegoxgarcia <diegoxgarcia@gmail.com>
2025-07-28 10:08:07 -0300

    wip

---
+++ Scenes/UI/game_menu.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+func _on_start_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_audio_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_how_to_button_pressed():
+	pass # Replace with function body.
+
+
+func _on_credit_button_pressed():
+	pass # Replace with function body.
